c28f5c1fea415bb0e0e6a06db14ad6eff12cbb67
aryanasp <aryanasp76@gmail.com>
2021-03-27 09:24:23 +0430

    Add lerp speed to stat bar values to change smoothly.

--- Assets/Scripts/StatUI.cs
+++ Assets/Scripts/StatUI.cs
@@ -13,6 +13,10 @@ public class StatUI : MonoBehaviour
     [SerializeField] private Image statBarImage;
     [SerializeField] private Text statBarText;
     private Dictionary<string, IStatController> _stats;
+    [SerializeField] private float lerpSpeed;
+    private bool _uiShouldUpdate = false;
+    private float _currentValue;
+    private float _maxValue;
 
     void Awake()
     {
@@ -27,9 +31,29 @@ public class StatUI : MonoBehaviour
         _stats[gameObject.tag].StatUpdater -= UpdateUI;
     }
 
+    void Update()
+    {
+        if (Math.Abs(statBarImage.fillAmount - _currentValue / _maxValue) < 0.01f)
+        {
+            _uiShouldUpdate = false;
+        }
+
+        if (_uiShouldUpdate)
+        {
+            LerpStatBar();
+        }
+    }
+    
     private void UpdateUI(float currentValue, float maxValue)
     {
         statBarText.text = currentValue + "/" + maxValue;
-        statBarImage.fillAmount = currentValue / maxValue;
+        _uiShouldUpdate = true;
+        _currentValue = currentValue;
+        _maxValue = maxValue;
+    }
+
+    private void LerpStatBar()
+    {
+        statBarImage.fillAmount = Mathf.Lerp(statBarImage.fillAmount , _currentValue / _maxValue, Time.deltaTime * lerpSpeed);
     }
 }
